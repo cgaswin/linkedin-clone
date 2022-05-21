b683202212f6a6e1bb6c0e9a4c2edb55c44d442c
AswinCG <cgaswin333@gmail.com>
2022-05-21 21:36:53 +0530

    working on feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./stylesheets/Feed.css";
 import CreateIcon from "@mui/icons-material/Create";
 import ImageIcon from "@mui/icons-material/Image";
@@ -6,16 +6,44 @@ import InputOption from "./InputOption";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
+import Post from "./Post";
+import { db } from "../config/firebase-config";
 
 const Feed = () => {
+
+    const [input,setInput] = useState("")
+    const [posts,setPosts] = useState([])
+
+    const sendPost = (e) => {
+        e.preventDefault();
+        db.collection("posts").add({
+          name: "Aswin CG",
+          description: "This is a test",
+          message: input,
+          photoUrl: "",
+        });
+
+    }
+
+    useEffect(()=>{
+        db.collection('posts').onSnapshot(snapshot => {
+            setPosts(snapshot.docs.map(doc => (
+                {
+                    id:doc.id,
+                    data:doc.data(),
+                }
+            )))
+        })
+    },[])
+
   return (
     <div className="feed">
       <div className="feed_input_container">
         <div className="feed_input">
           <CreateIcon />
           <form>
-            <input type="text" />
-            <button type="submit">Send</button>
+            <input value={input} onChange={(e)=>{setInput(e.target.value)}} type="text" />
+            <button onClick={sendPost} type="submit">Send</button>
           </form>
         </div>
 
@@ -32,7 +60,10 @@ const Feed = () => {
       </div>
 
         {/*posts*/}
-        
+        {posts.map((post)=>(
+            <Post/>
+        ))}
+        <Post name='Aswin CG' description="This is a test" message="wow this worked"/>
 
     </div>
   );
